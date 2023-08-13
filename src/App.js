05b37c62580953bbde16fe3d2e1fae152ae9bf4b
Bekalu <bekpostgrad@gmail.com>
2023-08-12 21:52:44 -0500

    added sidebar row component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header/Header";
+import Sidebar from "./Sidebar/Sidebar";
 
 function App() {
   return (
@@ -8,10 +9,11 @@ function App() {
       <div className="app">
         <Header />
 
-        {/* App body*/}
-        {/* SideBar*/}
-        {/* Feed*/}
-        {/* Widgets*/}
+        <div class="app__body">
+          <Sidebar />
+          {/* Feed*/}
+          {/* Widgets*/}
+        </div>
       </div>
     </>
   );
