9e23165510cfe58664f910fae011abed449e582a
Bekalu <bekpostgrad@gmail.com>
2023-08-14 16:03:05 -0500

    added side bar components

--- src/Sidebar/Sidebar.js
+++ src/Sidebar/Sidebar.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./Sidebar.css";
-import SidebarRow from "./SidebarRow/SidebarRow.js";
+import SidebarRow from "./SidebarRow/SidebarRow";
+
+import Chat from "@mui/icons-material/Chat";
+import People from "@mui/icons-material/People";
+import LocalHospital from "@mui/icons-material/LocalHospital";
+import MarketplaceIcon from "@mui/icons-material/Storefront";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      <SidebarRow />
-      <SidebarRow />
-      <SidebarRow />
-      <SidebarRow />
+      <SidebarRow
+        src="https://avatars.githubusercontent.com/BeTKH"
+        title="Beck Tkh"
+      />
+      <SidebarRow Icon={LocalHospital} title="Covid-19 Info Center" />
+      <SidebarRow Icon={People} title="Friends" />
+      <SidebarRow Icon={Chat} title="Messanger" />
+      <Sidebar Icon={MarketplaceIcon} title="Marketplace" />
     </div>
   );
 }
--- src/Sidebar/SidebarRow/SidebarRow.js
+++ src/Sidebar/SidebarRow/SidebarRow.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./SidebarRow.css";
+import Avatar from "@mui/material/Avatar";
 
-function SidebarRow() {
-  return <div className="sidebarRow">SidebarRow</div>;
+function SidebarRow({ src, Icon, title }) {
+  return (
+    <div className="sidebarRow">
+      {src && <Avatar src={src} />}
+      {Icon && <Icon />}
+      <h4>{title}</h4>
+    </div>
+  );
 }
 
 export default SidebarRow;
