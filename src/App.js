8180317566e207d123aa6f78030af2a43ef9eb73
Bekalu <bekpostgrad@gmail.com>
2023-09-20 10:58:08 -0500

    changed login creds

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Widgets from "./Widgets/Widgets";
 import Login from "./Login";
 
 function App() {
-  const user = "Bek";
+  const user = null;
   return (
     <div className="app">
       {!user ? (
